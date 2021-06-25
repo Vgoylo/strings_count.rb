f6346d5b1bc89caeaf847747a4888ad175e3b2fa
vgoylo <y_draiv.by@mail.ru>
2021-06-25 18:31:03 +0300

    Fix initiale commit

--- strings_count.rb
+++ strings_count.rb
@@ -2,4 +2,4 @@ def strings_count(arr,str)
   arr.select { |element| element.include?(str)  }
 end
 
-p strings_count([[1,2], ['vitaliy '],['tanya', 3], [3, 5, 8]], 'vitaliy')
+p strings_count([[1,2], ['vitaliy'],['tanya', 3], [3, 5, 8]], 'vitaliy')
